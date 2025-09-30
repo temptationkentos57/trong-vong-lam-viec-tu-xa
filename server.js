0b5bbc674022f06041e98469d7b480c0845eeedf
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-30 16:44:44 +0200

    Updated middleware to use express.json() instead of body-parser.

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // Updated middleware here
 
 // Connect to MongoDB
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/default_db';
